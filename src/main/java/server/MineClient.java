462bba89affe37490a7e26ec142a43296488ccd9
polesmih <polesmih@yandex.ru>
2021-07-06 12:40:31 +0300

    Homework lesson 1

---
+++ src/main/java/server/MineClient.java
@@ -0,0 +1,9 @@
+package server;
+
+import java.io.IOException;
+
+public class MineClient {
+    public static void main(String[] args) throws IOException {
+        new Client();
+    }
+}
--- src/main/java/server/ServerIO.java
+++ src/main/java/server/ServerIO.java
@@ -1,19 +1,69 @@
 package server;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerIO {
-    public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(8189);
-        System.out.println("Server started...");
-        while (true) {
-            Socket socket = server.accept();
-            System.out.println("Client accepted...");
-            try {
-                new Thread(new )
+
+
+    public final static int PORT = 8189;
+    public final static String PATH_TO_FILE = "C:/GB_Cloud-storage/Res/2.txt";
+    public final static int FILE_SIZE = 6000000;
+
+    public static void main(String[] args) {
+
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        int bytesRead;
+        int current = 0;
+        FileOutputStream fileOutputStream = null;
+        BufferedOutputStream bufferedOutputStream = null;
+
+        try {
+            serverSocket = new ServerSocket(PORT);
+
+            while (true) {
+                System.out.println("Waiting connection...");
+                try {
+                    // ожидание подключения
+                    socket = serverSocket.accept();
+                    System.out.println("Accepted connection: " + socket);
+
+                    // получение файла
+                    byte[] bytes = new byte[FILE_SIZE];
+                    InputStream inputStream = socket.getInputStream();
+                    fileOutputStream = new FileOutputStream(PATH_TO_FILE);
+                    bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+                    bytesRead = inputStream.read(bytes, 0, bytes.length);
+                    current = bytesRead;
+
+                    do {
+                        bytesRead = inputStream.read(bytes, current, (bytes.length - current));
+                        if (bytesRead >= 0)
+                            current += bytesRead;
+                    } while (bytesRead >= -1);
+
+                    bufferedOutputStream.write(bytes, 0, current);
+                    bufferedOutputStream.flush();
+
+                    System.out.println("File " + PATH_TO_FILE + " downloaded (" + current + "bytes read)");
+
+                } finally {
+                    if (bufferedOutputStream != null)
+                        bufferedOutputStream.close();
+                    if (fileOutputStream != null)
+                        fileOutputStream.close();
+                    if (socket != null)
+                        socket.close();
+                }
             }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 }
+
+
